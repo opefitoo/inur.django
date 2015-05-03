0cf7855d90e68478130414d97fbf887d0a3efeee
mehdi <mehdi.benammar@pragmaconsult.lu>
2015-05-03 20:37:17 +0200

    trying to solve DB issue

--- opefitoonursev2/settings.py
+++ opefitoonursev2/settings.py
@@ -120,3 +120,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+## got from inet: http://kencochrane.net/blog/2011/11/developers-guide-for-running-django-apps-on-heroku/
+import os, sys, urlparse
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+try:
+    if os.environ.has_key('DATABASE_URL'):
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        DATABASES['default'] = {
+            'NAME':     url.path[1:],
+            'USER':     url.username,
+            'PASSWORD': url.password,
+            'HOST':     url.hostname,
+            'PORT':     url.port,
+        }
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except:
+    print "Unexpected error:", sys.exc_info()
+
