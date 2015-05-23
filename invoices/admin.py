7930a225ad00b479ab091bbff69adc60ad89433f
mehdi <mehdi.benammar@pragmaconsult.lu>
2015-05-23 22:08:19 +0200

    add 2 invoices actions

--- invoices/admin.py
+++ invoices/admin.py
@@ -10,44 +10,60 @@ class CareCoreAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'gross_amount')
     search_fields = ['code', 'name']
 
+
 admin.site.register(CareCode, CareCoreAdmin)
+
 
 class PatientAdmin(admin.ModelAdmin):
     list_filter = ('city',)
     list_display = ('name', 'first_name', 'phone_number', 'code_sn', 'participation_statutaire')
     search_fields = ['name', 'first_name', 'code_sn']
+
+
 admin.site.register(Patient, PatientAdmin)
-    
+
+
 class PrestationAdmin(AjaxSelectAdmin):
     from invaction import create_invoice
+
     date_hierarchy = 'date'
     list_display = ('patient', 'carecode', 'date')
     search_fields = ['patient__name', 'patient__first_name']
     list_filter = ('patient__name',)
     actions = [create_invoice]
-    form = make_ajax_form( Prestation, {'patient': 'patient', 'carecode' : 'carecode'})
+    form = make_ajax_form(Prestation, {'patient': 'patient', 'carecode': 'carecode'})
+
+
 admin.site.register(Prestation, PrestationAdmin)
-        
+
+
 class InvoiceItemAdmin(AjaxSelectAdmin):
     from invoices.action import export_to_pdf
     from invoices.action_private_participation import pdf_private_invoice
-    from invaction import previous_months_invoices_october
+    from invaction import previous_months_invoices_october, previous_months_invoices_november, previous_months_invoices_december
+
     date_hierarchy = 'invoice_date'
     list_display = ('invoice_number', 'patient', 'invoice_month', 'prestations_invoiced', 'invoice_sent' )
-    list_filter =  ['invoice_date', 'patient__name', 'invoice_sent']
+    list_filter = ['invoice_date', 'patient__name', 'invoice_sent']
     search_fields = ['patient']
-    actions = [pdf_private_invoice, export_to_pdf,  previous_months_invoices_october ]
-    form = make_ajax_form(InvoiceItem,{'patient':'patient_du_mois'})
+    actions = [pdf_private_invoice, export_to_pdf, previous_months_invoices_october, previous_months_invoices_november, previous_months_invoices_december]
+    form = make_ajax_form(InvoiceItem, {'patient': 'patient_du_mois'})
+
+
 admin.site.register(InvoiceItem, InvoiceItemAdmin)
+
 
 class PrivateInvoiceItemAdmin(AjaxSelectAdmin):
     from action_private import pdf_private_invoice
+
     date_hierarchy = 'invoice_date'
     list_display = ('invoice_number', 'private_patient', 'invoice_month', 'prestations_invoiced', 'invoice_sent' )
-    list_filter =  ['invoice_date', 'private_patient__name', 'invoice_sent']
+    list_filter = ['invoice_date', 'private_patient__name', 'invoice_sent']
     search_fields = ['private_patient']
     actions = [pdf_private_invoice]
-    form = make_ajax_form(PrivateInvoiceItem,{'private_patient':'private_patient_a_facturer'})
+    form = make_ajax_form(PrivateInvoiceItem, {'private_patient': 'private_patient_a_facturer'})
+
+
 admin.site.register(PrivateInvoiceItem, PrivateInvoiceItemAdmin)
 
 
