2a6e0ea146cc80bcbb86bd9cb7acaa58a8397e65
mehdi <mehdi.benammar@theb2group.net>
2016-01-08 16:22:43 +0100

    add action for may invoice's generation

--- invoices/invaction.py
+++ invoices/invaction.py
@@ -51,6 +51,29 @@ def previous_months_invoices_april(modeladmin, request, queryset):
         invoiceitem.save()
         invoice_counters = invoice_counters + 1
 
+def previous_months_invoices_may(modeladmin, request, queryset):
+    # response = HttpResponse(content_type='text')
+
+    previous_month_patients = Patient.objects.raw("select p.id, p.name, p.first_name " +
+                                                  "from public.invoices_patient p, public.invoices_prestation prest " +
+                                                  "where p.id = prest.patient_id " +
+                                                  "and prest.date >= '2015-05-01'and prest.date <= '2015-05-31' " +
+                                                  "and p.private_patient = 'f' " +
+                                                  "and (select count(inv.id) from public.invoices_invoiceitem inv " +
+                                                  "where inv.invoice_date between '2015-05-01'::DATE and '2015-05-31'::DATE " +
+                                                  "and inv.patient_id = p.id) = 0" +
+                                                  "group by p.id " +
+                                                  "order by p.name")
+    invoice_counters = 0
+    for p in previous_month_patients:
+        invoiceitem = InvoiceItem(patient=p,
+                                  invoice_date=datetime.datetime(2015, 05, 31),
+                                  invoice_sent=False,
+                                  invoice_paid=False)
+        invoiceitem.clean()
+        invoiceitem.save()
+        invoice_counters = invoice_counters + 1
+
 def create_invoice(modeladmin, request, queryset):
     # response = HttpResponse(content_type='text')
 
