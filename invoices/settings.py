a75f3287f3ff458484103df854f666c7b1e2346a
mehdi <mehdi@benammar.com>
2016-02-25 17:43:01 +0100

    activate all apps

--- invoices/settings.py
+++ invoices/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'invoices',
-    #'ajax_select'
+    'invoices',
+    'ajax_select'
 )
 
 MIDDLEWARE_CLASSES = (
