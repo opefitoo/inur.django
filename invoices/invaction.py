434590dd7ded1822543df622577201b8f2e3dcc4
mehdi <mehdi@benammar.com>
2016-04-27 16:23:11 +0200

    invoice generation actions for august and sept

--- invoices/invaction.py
+++ invoices/invaction.py
@@ -6,23 +6,47 @@ from models import Patient, InvoiceItem, Prestation, PrivateInvoiceItem
 from django.http import HttpResponseRedirect
 
 
-def previous_months_invoices_july(modeladmin, request, queryset):
+def previous_months_invoices_august(modeladmin, request, queryset):
     # response = HttpResponse(content_type='text')
 
     previous_month_patients = Patient.objects.raw("select p.id, p.name, p.first_name " +
                                                   "from public.invoices_patient p, public.invoices_prestation prest " +
                                                   "where p.id = prest.patient_id " +
-                                                  "and prest.date >= '2015-07-01'and prest.date <= '2015-07-31' " +
+                                                  "and prest.date >= '2015-08-01'and prest.date <= '2015-08-31' " +
                                                   "and p.private_patient = 'f' " +
                                                   "and (select count(inv.id) from public.invoices_invoiceitem inv " +
-                                                  "where inv.invoice_date between '2015-07-01'::DATE and '2015-07-31'::DATE " +
+                                                  "where inv.invoice_date between '2015-08-01'::DATE and '2015-08-31'::DATE " +
                                                   "and inv.patient_id = p.id) = 0" +
                                                   "group by p.id " +
                                                   "order by p.name")
     invoice_counters = 0
     for p in previous_month_patients:
         invoiceitem = InvoiceItem(patient=p,
-                                  invoice_date=datetime.datetime(2015, 07, 31),
+                                  invoice_date=datetime.datetime(2015, 8, 31),
+                                  invoice_sent=False,
+                                  invoice_paid=False)
+        invoiceitem.clean()
+        invoiceitem.save()
+        invoice_counters = invoice_counters + 1
+
+
+def previous_months_invoices_september(modeladmin, request, queryset):
+    # response = HttpResponse(content_type='text')
+
+    previous_month_patients = Patient.objects.raw("select p.id, p.name, p.first_name " +
+                                                  "from public.invoices_patient p, public.invoices_prestation prest " +
+                                                  "where p.id = prest.patient_id " +
+                                                  "and prest.date >= '2015-09-01'and prest.date <= '2015-09-30' " +
+                                                  "and p.private_patient = 'f' " +
+                                                  "and (select count(inv.id) from public.invoices_invoiceitem inv " +
+                                                  "where inv.invoice_date between '2015-09-01'::DATE and '2015-09-30'::DATE " +
+                                                  "and inv.patient_id = p.id) = 0" +
+                                                  "group by p.id " +
+                                                  "order by p.name")
+    invoice_counters = 0
+    for p in previous_month_patients:
+        invoiceitem = InvoiceItem(patient=p,
+                                  invoice_date=datetime.datetime(2015, 9, 30),
                                   invoice_sent=False,
                                   invoice_paid=False)
         invoiceitem.clean()
