ff66c87fedf922989b424055313082e7d95dfa94
mehdi <mehdi@benammar.com>
2016-05-30 13:36:46 +0200

    invoice action for october

--- invoices/admin.py
+++ invoices/admin.py
@@ -74,13 +74,13 @@ class InvoiceItemAdmin(AjaxSelectAdmin):
     from invoices.action import export_to_pdf
     from invoices.action_private import pdf_private_invoice
     from action_private_with_recap import pdf_private_invoice_with_recap
-    from invaction import previous_months_invoices_august, previous_months_invoices_september
+    from invaction import previous_months_invoices_august, previous_months_invoices_october
 
     date_hierarchy = 'invoice_date'
     list_display = ('invoice_number', 'patient', 'invoice_month', 'prestations_invoiced', 'invoice_sent',  )
     list_filter = ['invoice_date', 'patient__name', 'invoice_sent']
     search_fields = ['patient']
-    actions = [pdf_private_invoice, export_to_pdf, pdf_private_invoice_with_recap, previous_months_invoices_august, previous_months_invoices_september]
+    actions = [pdf_private_invoice, export_to_pdf, pdf_private_invoice_with_recap, previous_months_invoices_august, previous_months_invoices_october]
     form = make_ajax_form(InvoiceItem, {'patient': 'patient_du_mois'})
 
 
