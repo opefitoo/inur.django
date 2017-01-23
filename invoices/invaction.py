828c5fed9982bb49fd0b0a140c4ba97cf57100bf
mehdi <mehdi@benammar.com>
2017-01-23 19:03:28 +0100

    action for december 2016

--- invoices/invaction.py
+++ invoices/invaction.py
@@ -29,23 +29,23 @@ def previous_months_invoices_october(modeladmin, request, queryset):
         invoice_counters = invoice_counters + 1
 
 
-def previous_months_invoices_nov(modeladmin, request, queryset):
+def previous_months_invoices_dec(modeladmin, request, queryset):
     # response = HttpResponse(content_type='text')
 
     previous_month_patients = Patient.objects.raw("select p.id, p.name, p.first_name " +
                                                   "from public.invoices_patient p, public.invoices_prestation prest " +
                                                   "where p.id = prest.patient_id " +
-                                                  "and prest.date >= '2016-11-01'and prest.date <= '2016-11-30' " +
+                                                  "and prest.date >= '2016-12-01'and prest.date <= '2016-12-31' " +
                                                   "and p.private_patient = 'f' " +
                                                   "and (select count(inv.id) from public.invoices_invoiceitem inv " +
-                                                  "where inv.invoice_date between '2016-11-01'::DATE and '2016-11-30'::DATE " +
+                                                  "where inv.invoice_date between '2016-12-01'::DATE and '2016-12-31'::DATE " +
                                                   "and inv.patient_id = p.id) = 0" +
                                                   "group by p.id " +
                                                   "order by p.name")
     invoice_counters = 0
     for p in previous_month_patients:
         invoiceitem = InvoiceItem(patient=p,
-                                  invoice_date=datetime.datetime(2016, 11, 30),
+                                  invoice_date=datetime.datetime(2016, 12, 31),
                                   invoice_sent=False,
                                   invoice_paid=False)
         invoiceitem.clean()
