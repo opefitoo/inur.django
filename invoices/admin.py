da9d5094ff3bfe59dadeb5e495b29edbe2ec3de2
Mehdi Benammar <mehdi@benammar.com>
2017-09-24 21:36:20 +0200

    invoices june 2017

--- invoices/admin.py
+++ invoices/admin.py
@@ -75,7 +75,7 @@ class InvoiceItemAdmin(AjaxSelectAdmin):
     from invoices.action import export_to_pdf
     from action_private import pdf_private_invoice
     from action_private_participation import pdf_private_invoice_pp
-    from invaction import previous_months_invoices_april,previous_months_invoices_may_2017
+    from invaction import previous_months_invoices_april,previous_months_invoices_june_2017
     from generate_pacifico_invoices import niedercorn_avril_mai_2017
     from invaction import syncro_clients
     date_hierarchy = 'invoice_date'
@@ -83,7 +83,7 @@ class InvoiceItemAdmin(AjaxSelectAdmin):
     list_display = ('invoice_number', 'patient', 'invoice_month', 'invoice_sent')
     list_filter = ['invoice_date', 'patient__name', 'invoice_sent']
     search_fields = ['patient']
-    actions = [export_to_pdf, pdf_private_invoice_pp, pdf_private_invoice, syncro_clients,previous_months_invoices_april,previous_months_invoices_may_2017,niedercorn_avril_mai_2017]
+    actions = [export_to_pdf, pdf_private_invoice_pp, pdf_private_invoice, syncro_clients, previous_months_invoices_april, previous_months_invoices_june_2017, niedercorn_avril_mai_2017]
     form = make_ajax_form(InvoiceItem, {'patient': 'patient_du_mois'})
 
 admin.site.register(InvoiceItem, InvoiceItemAdmin)
