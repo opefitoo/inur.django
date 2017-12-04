d4856dd0d192d20fdabc0022ea3d0582376ab372
Alex Olkhovskii <alex.olkhovskii@gmail.com>
2017-12-04 15:37:33 +0200

    Autocomplete light - fix inline form adding issue

--- invoices/static/js/widgets/autocomplete-light-custom.js
+++ invoices/static/js/widgets/autocomplete-light-custom.js
@@ -1,27 +1,44 @@
 $(function() {
+    $('.inline-group').on('click', '.add-row a', function() {
+        replaceJetSelectWithAutocompleteLight($(this).closest('.inline-group'));
+    });
+
     // Remove jet select2 if autocomplete-light is used
-    var autocomplete_light_selects = $('select[data-is_custom_autocomplete_light="true"]');
-    if (0 < autocomplete_light_selects.length) {
-        $.each(autocomplete_light_selects, function(index, item) {
-            var parent = $(item).parent();
-            parent.find('.select2-container--jet').remove();
-            var autocomplete_light = parent.find('.select2-container--default');
-            autocomplete_light.removeClass('select2-container--default').addClass('select2-container--jet');
+    function replaceJetSelectWithAutocompleteLight(parent) {
+        var autocomplete_light_selects;
+        if ('undefined' != typeof(parent)) {
+            autocomplete_light_selects = parent.find('select[data-is_custom_autocomplete_light="true"]');
+        }
+        else {
+            autocomplete_light_selects = $('select[data-is_custom_autocomplete_light="true"]');
+        }
 
-            $(item).on("select2:open", function() {
-                $('.select2-container--default').removeClass('select2-container--default').addClass('select2-container--jet');
-            });
+        if (0 < autocomplete_light_selects.length) {
+            $.each(autocomplete_light_selects, function(index, item) {
+                var parent = $(item).parent();
 
-            var prefix = $(item).getFormPrefix();
-            var full_field_name = $(item).prop('name');
-            var field_name = full_field_name.replace(prefix, '');
+                parent.find('.select2-container--jet').not('.custom').remove();
+                var autocomplete_light = parent.find('.select2-container--default');
 
-            $.each($(item).data('forward'), function(index, name){
-                $('[name$="'+name+'"]').on('change', function() {
-                    var prefix = $(this).getFormPrefix();
-                    $('[name='+prefix+field_name+']').val(null).trigger('change');
+                autocomplete_light.removeClass('select2-container--default').addClass('select2-container--jet custom');
+
+                $(item).on("select2:open", function() {
+                    $('.select2-container--default').removeClass('select2-container--default').addClass('select2-container--jet');
+                });
+
+                var prefix = $(item).getFormPrefix();
+                var full_field_name = $(item).prop('name');
+                var field_name = full_field_name.replace(prefix, '');
+
+                $.each($(item).data('forward'), function(index, name) {
+                    $('[name$="' + name + '"]').on('change', function() {
+                        var prefix = $(this).getFormPrefix();
+                        $('[name=' + prefix + field_name + ']').val(null).trigger('change');
+                    });
                 });
             });
-        });
+        }
     }
+
+    replaceJetSelectWithAutocompleteLight();
 });
