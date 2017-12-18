e259f5c41f80e6a705c5b7219a799481399f0864
Alex Olkhovskii <alex.olkhovskii@gmail.com>
2017-12-18 22:34:58 +0200

    MedicalPrescription - test for file path

--- invoices/tests/models/test_medicalprescription.py
+++ invoices/tests/models/test_medicalprescription.py
@@ -1,7 +1,10 @@
+import os
+
 from django.utils import timezone
 from django.test import TestCase
 
-from invoices.models import Physician, MedicalPrescription, Patient
+from invoices.models import Physician, MedicalPrescription, Patient, update_medical_prescription_filename
+from invoices.storages import CustomizedGoogleDriveStorage
 
 
 class MedicalPrescriptionTestCase(TestCase):
@@ -43,3 +46,22 @@ class MedicalPrescriptionTestCase(TestCase):
                                            patient=patient)
         self.assertEqual(prescription.file_description, '%s %s %s' % (
         prescription.patient.name, prescription.patient.first_name, str(prescription.date)))
+
+
+class UpdateMedicalPrescriptionFilenameTestCase(TestCase):
+    def test_filename(self):
+        filename = 'somename.jpg'
+        date = timezone.now()
+        patient = Patient(first_name='first name',
+                          name='name')
+        prescription = MedicalPrescription(date=date,
+                                           patient=patient)
+
+        path = os.path.join(CustomizedGoogleDriveStorage.MEDICAL_PRESCRIPTION_FOLDER, str(prescription.date.year))
+        file_name, file_extension = os.path.splitext(filename)
+        filename = '%s_%s_%s%s' % (
+            prescription.patient.name, prescription.patient.first_name, str(prescription.date), file_extension)
+        expected_name = os.path.join(path, filename)
+
+        generated_name = update_medical_prescription_filename(prescription, filename)
+        self.assertEqual(generated_name, expected_name)
