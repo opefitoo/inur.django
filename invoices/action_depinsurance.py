3025ca308b37ff922a2d84cc8e9c6bf32d0a48a7
mehdi benammar <mehdi@benammar.com>
2019-03-22 15:46:48 +0100

    fix paymment reference for single files

--- invoices/action_depinsurance.py
+++ invoices/action_depinsurance.py
@@ -12,7 +12,6 @@ def export_to_pdf2(modeladmin, request, queryset):
     # Append invoice number and invoice date
     if len(queryset) != 1:
         _file_name = '-'.join([a.invoice_number for a in queryset.order_by("invoice_number")])
-        payment_ref = hashlib.sha1(_file_name.encode("UTF-8")).hexdigest()[:10]
 
         response['Content-Disposition'] = 'attachment; filename="invoice%s.pdf"' % (_file_name.replace(" ", "")[:150])
     else:
@@ -21,6 +20,7 @@ def export_to_pdf2(modeladmin, request, queryset):
                                                                                            queryset[
                                                                                                0].invoice_date.strftime(
                                                                                                '%d-%m-%Y'))
+    payment_ref = hashlib.sha1(response['Content-Disposition'].encode("UTF-8")).hexdigest()[:10]
 
     doc = SimpleDocTemplate(response, rightMargin=2 * cm, leftMargin=2 * cm, topMargin=1 * cm, bottomMargin=1 * cm)
     elements = get_doc_elements(queryset, payment_ref)
