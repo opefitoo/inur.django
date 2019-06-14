b68539e22774e2c8c291f41399923e4957bc4cc2
mehdi benammar <mehdi@benammar.com>
2019-06-14 13:34:44 +0200

    fix migration script

--- invoices/migrations/0064_jan2019_import_cns_prices.py
+++ invoices/migrations/0064_jan2019_import_cns_prices.py
@@ -40,7 +40,7 @@ def process_codes(apps, schema_editor):
                             v.gross_amount = row[3].replace(',', '.')
                             v.end_date = end_date
                             v.save()
-                            updated_codes.append(','.join(care_code_to_updt.code + v.end_date + v.start_date))
+                            updated_codes.append('%s from %s to %s' % (care_code_to_updt.code, v.end_date, v.start_date))
                         elif v.end_date is not None and v.end_date < start_date and v.start_date < start_date:
                             codes_that_are_too_old.append('%s from %s to %s' % (care_code_to_updt.code, v.start_date,
                                                                                 v.end_date))
