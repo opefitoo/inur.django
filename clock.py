eebd37f6a23deaa5efb65d7adb8d6af6566a0333
ahmed <a.amadou.yahoo@gmail.com>
2020-06-27 19:57:09 +0100

    Handeling events try3

--- clock.py
+++ clock.py
@@ -1,8 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     os.system('python manage.py check_events')
 
