6f7be4a92f284ef1d356288d8e87b30c43b30fa8
mehdi benammar <mehdi@benammar.com>
2020-07-01 20:20:11 +0200

    german translations

--- clock.py
+++ clock.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 
-sched = BlockingScheduler()
+scheduler = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
+
+@scheduler.scheduled_job('interval', minutes=1)
 def timed_job():
     os.system('python manage.py check_events')
 
 
-sched.start()
+scheduler.start()
