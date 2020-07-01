a7742fae71c7ae9a850eddaa0d7467af9e3a75da
mehdi benammar <mehdi@benammar.com>
2020-07-01 21:09:28 +0200

    make django locale aware

--- invoices/settings.py
+++ invoices/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
