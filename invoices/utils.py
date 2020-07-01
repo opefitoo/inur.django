f6450a09278e30f5666d5a1518da52dbf1737a1f
mehdi benammar <mehdi@benammar.com>
2020-07-01 21:11:11 +0200

    file formatting

--- invoices/utils.py
+++ invoices/utils.py
@@ -1,6 +1,5 @@
 __author__ = 'mehdi'
 
-
 ## credit goes to: http://stackoverflow.com/questions/18593661/how-do-i-strftime-a-date-object-in-a-different-locale
 
 import locale
@@ -9,12 +8,10 @@ import threading
 from contextlib import contextmanager
 
 from calendar import HTMLCalendar
-from datetime import datetime as dtime, date, time
-import datetime
 from invoices.events import Event
 
-
 LOCALE_LOCK = threading.Lock()
+
 
 @contextmanager
 def setlocale(name):
@@ -25,11 +22,12 @@ def setlocale(name):
         finally:
             locale.setlocale(locale.LC_ALL, saved)
 
+
 class EventCalendar(HTMLCalendar):
     def __init__(self, events=None):
         super(EventCalendar, self).__init__()
         self.events = events
- 
+
     def formatday(self, day, weekday, events):
         """
         Return a day as a table cell.
@@ -39,26 +37,25 @@ class EventCalendar(HTMLCalendar):
         for event in events_from_day:
             events_html += event.get_absolute_url() + "<br>"
         events_html += "</ul>"
- 
         if day == 0:
             return '<td class="noday">&nbsp;</td>'  # day outside month
         else:
             return '<td class="%s">%d%s</td>' % (self.cssclasses[weekday], day, events_html)
- 
+
     def formatweek(self, theweek, events):
         """
         Return a complete week as a table row.
         """
         s = ''.join(self.formatday(d, wd, events) for (d, wd) in theweek)
         return '<tr>%s</tr>' % s
- 
+
     def formatmonth(self, theyear, themonth, withyear=True):
         """
         Return a formatted month as a table.
         """
- 
+
         events = Event.objects.filter(day__month=themonth)
- 
+
         v = []
         a = v.append
         a('<table border="0" cellpadding="0" cellspacing="0" class="month">')
