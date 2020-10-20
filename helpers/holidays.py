e66026e955058294108ff268e16dd520b998ad0d
BENAMMAR Mehdi <mehdi@benammar.com>
2020-10-20 17:22:33 +0200

    holidays that are not of type 1 and 2 cannot be added to timesheet total hours

--- helpers/holidays.py
+++ helpers/holidays.py
@@ -5,12 +5,13 @@ from invoices.holidays import HolidayRequest
 
 
 def how_many_hours_taken_in_period(data):
+    # TODO : remove hard coded values for reason filtering
     holiday_requests = HolidayRequest.objects.filter(
         Q(start_date__range=(data['start_date'], data['end_date'])) |
         Q(end_date__range=(data['start_date'], data['end_date'])) |
         Q(start_date__lte=data['start_date'], end_date__gte=data['start_date']) |
         Q(start_date__lte=data['end_date'], end_date__gte=data['end_date'])
-    ).filter(employee_id=data['user_id']).filter(request_accepted=True)
+    ).filter(employee_id=data['user_id']).filter(request_accepted=True).filter(reason__range=(1, 2))
     if len(holiday_requests) > 0:
         return len(holiday_requests) * Employee.objects.get(user_id=data['user_id']).employeecontractdetail_set.filter(
             start_date__lte=data['start_date']).first().number_of_hours / 5
