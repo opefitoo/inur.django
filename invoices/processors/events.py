7d37da8c214c16f06a473f5e9f3131b2b2de730a
Benammar Mehdi <mehdi@benammar.com>
2021-02-06 13:54:37 +0100

    optimize deletion

--- invoices/processors/events.py
+++ invoices/processors/events.py
@@ -4,8 +4,12 @@ PLANNING_SCRIPT_CREATOR_NAME = "planning script"
 
 
 def delete_events_created_by_script(year: int, month: int):
-    events_to_delete_dict = Event.objects.filter(day__year=year, day__month=month,
-                                                 created_by=PLANNING_SCRIPT_CREATOR_NAME)
-    for e in events_to_delete_dict:
+    return_events_to_delete_dict = []
+    events_to_delete = Event.objects.filter(day__year=year, day__month=month,
+                                                        created_by=PLANNING_SCRIPT_CREATOR_NAME)
+
+    for e in events_to_delete:
+        return_events_to_delete_dict.append(e)
         e.delete()
-    return events_to_delete_dict
+    events_to_delete.delete()
+    return return_events_to_delete_dict;
