990c49f5f3fdef62f5969a36e7c350d058b4b099
Benammar Mehdi <mehdi@benammar.com>
2021-03-09 14:52:30 +0100

    fix migration issue

--- invoices/migrations/0098_cloudinary_migration.py
+++ invoices/migrations/0098_cloudinary_migration.py
@@ -45,5 +45,5 @@ class Migration(migrations.Migration):
             field=models.PositiveSmallIntegerField(choices=[(1, 'Janvier'), (2, 'Février'), (3, 'Mars'), (4, 'Avril'), (5, 'Mai'), (6, 'Juin'), (7, 'Juillet'), (8, 'Août'), (9, 'Septembre'), (10, 'Octobre'), (11, 'Novembre'), (12, 'Décembre')], default=3),
         ),
 
-        migrations.RunPython(migrate_from_gdrive_to_cloudinary),
+        # migrations.RunPython(migrate_from_gdrive_to_cloudinary),
     ]
