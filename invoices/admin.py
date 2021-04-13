4368e9f37a6f0d8f0774b333554e30cd227b6074
Benammar Mehdi <mehdi@benammar.com>
2021-04-13 19:40:58 +0200

    remove dangerous migration action

--- invoices/admin.py
+++ invoices/admin.py
@@ -168,89 +168,10 @@ class BiographyHabitsInLine(admin.TabularInline):
     # fk_name = 'biography'
 
 
-def copy_to_new(modeladmin, request, queryset):
-    olds = PatientAnamnesis.objects.all()
-    for old in olds:
-        new = dependence.models.PatientAnamnesis()
-        for k, v in old.__dict__.items():
-            new.__dict__[k] = copy.deepcopy(v)
-        # new = copy.deepcopy(old)
-        new.id = None
-        new.save()
-    # # AssignedPhysician
-    # olds = AssignedPhysician.objects.all()
-    # for old in olds:
-    #     new = dependence.models.AssignedPhysician(old.__dict__)
-    #     for k, v in old.__dict__.items():
-    #         new.__dict__[k] = copy.deepcopy(v)
-    #         new = copy.deepcopy(old)
-    #         new.save()
-
-
-
-def duplicate(obj, value=None, field=None, duplicate_order=None):
-    """
-    Duplicate all related objects of obj setting
-    field to value. If one of the duplicate
-    objects has an FK to another duplicate object
-    update that as well. Return the duplicate copy
-    of obj.
-    duplicate_order is a list of models which specify how
-    the duplicate objects are saved. For complex objects
-    this can matter. Check to save if objects are being
-    saved correctly and if not just pass in related objects
-    in the order that they should be saved.
-    """
-    from django.db.models.deletion import Collector
-    from django.db.models.fields.related import ForeignKey
-
-    collector = Collector(using='default')
-    collector.collect([obj])
-    collector.sort()
-    related_models = collector.data.keys()
-    data_snapshot = {}
-    for key in collector.data.keys():
-        data_snapshot.update(
-            {key: dict(zip([item.pk for item in collector.data[key]], [item for item in collector.data[key]]))})
-    root_obj = None
-
-    # Sometimes it's good enough just to save in reverse deletion order.
-    if duplicate_order is None:
-        duplicate_order = reversed(related_models)
-
-    for model in duplicate_order:
-        # Find all FKs on model that point to a related_model.
-        fks = []
-        for f in model._meta.fields:
-            if isinstance(f, ForeignKey) and f.remote_field.related_model in related_models:
-                fks.append(f)
-        # Replace each `sub_obj` with a duplicate.
-        if model not in collector.data:
-            continue
-        sub_objects = collector.data[model]
-        for obj in sub_objects:
-            for fk in fks:
-                fk_value = getattr(obj, "%s_id" % fk.name)
-                # If this FK has been duplicated then point to the duplicate.
-                fk_rel_to = data_snapshot[fk.remote_field.related_model]
-                if fk_value in fk_rel_to:
-                    dupe_obj = fk_rel_to[fk_value]
-                    setattr(obj, fk.name, dupe_obj)
-            # Duplicate the object and save it.
-            obj.id = None
-            if field is not None:
-                setattr(obj, field, value)
-            obj.save()
-            if root_obj is None:
-                root_obj = obj
-    return root_obj
-
 @admin.register(PatientAnamnesis)
 class PatientAnamnesisAdmin(ModelAdminObjectActionsMixin, FieldsetsInlineMixin, admin.ModelAdmin):
     list_display = ('patient', 'display_object_actions_list',)
     autocomplete_fields = ['patient']
-
-    actions = [copy_to_new]
 
     object_actions = [
         {
