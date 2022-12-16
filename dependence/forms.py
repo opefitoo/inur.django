11ae1b9a266c354114b4c98885aea641b21c7a6f
Ahmed Abdul Rahuman <a.amadou.yahoo@gmail.com>
2022-12-16 23:06:23 +0100

    added validation for multiple choices fields and some missing translations

--- dependence/forms.py
+++ dependence/forms.py
@@ -6,6 +6,7 @@ from django import forms
 from dependence.enums.falldeclaration_enum import FallCircumstances
 
 from dependence.falldeclaration import FallDeclaration
+from django.utils.translation import gettext_lazy as _
 
 class TypeDescriptionGenericInlineFormset(BaseInlineFormSet):
 
@@ -52,17 +53,47 @@ class FallDeclarationForm(forms.ModelForm):
         model = FallDeclaration
         exclude = ()
         
-
     def clean(self):
         fall_circumstance = self.cleaned_data.get('fall_circumstance', None)
         activity_name = self.cleaned_data.get('other_fall_circumstance', None)
         if fall_circumstance == FallCircumstances.FCI_OTHER_CAUSES:
             if activity_name in EMPTY_VALUES:
                 self._errors['other_fall_circumstance'] = self.error_class([
-                    'Selon votre sélection, il faut remplire le champ: "Autre circonstances de la chute"'])
+                    _("Depending on your selection, you must fill in the field: \"Other circumstances of the fall\"")])
         else:
             if activity_name not in EMPTY_VALUES:
                 self._errors['other_fall_circumstance'] = self.error_class([
-                    'Selon votre sélection, il faut laisser champ "Autre circonstances de la chute" vide'])
+                    _("Depending on your selection, you must leave the \"Other circumstances of the fall\" field empty")])
+
+        inline_fields=(
+                       'fall_consequences',
+                       'fall_required_medical_acts',
+                       'fall_cognitive_mood_diorders',
+                       'fall_incontinences'
+                       )
+        inline_fields_display=(
+                       _("Consequences of the fall"),
+                       _("Medical and/or nursing acts required within 24 hours"),
+                       _("Cognitive and/or mood disorders"),
+                       _("Incontinence")
+                        )               
+        inline_fields_name=(
+                       'consequence',
+                       'required_medical_act',
+                       'cognitive_mood_diorder',
+                       'incontinence'
+                        )               
+        for idx,field in enumerate(inline_fields):               
+            value_count = int(self.data.get(field+'-TOTAL_FORMS', 0))
+            temp = []
+            for i in range(0, value_count):
+                try:
+                    value = self.data.get((field+'-{0}-'+inline_fields_name[idx]).format(i), '')
+                except ValueError:
+                    pass
+                if value in temp:
+                    raise forms.ValidationError(_("You have a duplicate choice in the \"%s\", you can\'t select the same choice twice")% inline_fields_display[idx] )
+                else:
+                    temp.append(value)      
 
         return self.cleaned_data
