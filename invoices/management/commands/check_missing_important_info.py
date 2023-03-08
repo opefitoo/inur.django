8859c7db922355f983f7347038949e183a496e7c
Ahmed Abdul Rahuman <a.amadou.yahoo@gmail.com>
2023-03-08 13:36:57 +0100

    use important instead of mandatory

--- invoices/management/commands/check_missing_important_info.py
+++ invoices/management/commands/check_missing_important_info.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
 
-from invoices.processors.missing_infos import search_for_missing_manadatory_infos
+from invoices.processors.missing_infos import search_for_missing_important_infos
 
 
 class Command(BaseCommand):
-    help = 'Checks for missing manadatory information'
+    help = 'Checks for missing important information'
 
     def handle(self, *args, **options):
-        search_result = search_for_missing_manadatory_infos()
+        search_result = search_for_missing_important_infos()
         self.stdout.write(self.style.SUCCESS('Process result %s') % search_result)
         return
--- invoices/processors/missing_infos.py
+++ invoices/processors/missing_infos.py
@@ -3,17 +3,17 @@ from django.db.models import  Q
 from django.urls import reverse
 
 DEFAULT_APP_LABEL = 'invoices'
-DEFAULT_MANADATORY_FIELDS = [
+DEFAULT_IMPORTANT_FIELDS = [
     ('employee','driving_licence_number')
 ]
 BASE_URL = 'http://127.0.0.1:8000'
 
-def search_for_missing_manadatory_infos(manadatory_fields = DEFAULT_MANADATORY_FIELDS, app_label=DEFAULT_APP_LABEL):
+def search_for_missing_important_infos(important_fields = DEFAULT_IMPORTANT_FIELDS, app_label=DEFAULT_APP_LABEL):
     rslt=[]
-    for field_info in manadatory_fields:
+    for field_info in important_fields:
         field_model = field_info[0]
         field_name = field_info[1]
-        records_with_missing_infos = search_for_missing_manadatory_infos_for_field(field_model, field_name, app_label)
+        records_with_missing_infos = search_for_missing_important_infos_for_field(field_model, field_name, app_label)
         field_verbose_name = records_with_missing_infos.model._meta.get_field(field_name)._verbose_name
         field_model_verbose_name = records_with_missing_infos.model._meta.verbose_name
         for rec in records_with_missing_infos:
@@ -23,13 +23,13 @@ def search_for_missing_manadatory_infos(manadatory_fields = DEFAULT_MANADATORY_F
             rslt.append(message)
             info = (app_label, field_model)
             rec_admin_url = reverse('admin:%s_%s_change' % info, args=(rec.pk,))
-            message_for_link = "You can update the missing info bu following the link %s%s" % (BASE_URL,rec_admin_url)
+            message_for_link = "You can update the missing info by following the link %s%s" % (BASE_URL,rec_admin_url)
             rslt.append(message_for_link)
         
 
     return rslt
 
-def search_for_missing_manadatory_infos_for_field(field_model, field_name, app_label):
+def search_for_missing_important_infos_for_field(field_model, field_name, app_label):
     my_model = apps.get_model(app_label,model_name=field_model)
     field_name_isnull = field_name+'__isnull'
     srch = my_model.objects.filter(Q(**{field_name_isnull:True}) | Q(**{field_name:''}))
