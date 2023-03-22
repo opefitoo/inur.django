c63b8336ed29807ded2b1d146e5e6b727399643f
BenaoudaMamcha <b.mamchaoui13@gmail.com>
2023-03-22 21:45:48 +0100

    fall_declaration_pdf_print

--- invoices/settings.py
+++ invoices/settings.py
@@ -342,17 +342,17 @@ if 'GOOGLE_APPLICATION_CREDENTIALS2' in os.environ and not os.path.exists(GOOGLE
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(
-    dsn="https://8e73556ef21c4c73a6ecec31b9d742cc@o4504561450287104.ingest.sentry.io/4504561450287104",
-    integrations=[
-        DjangoIntegration(),
-    ],
+# sentry_sdk.init(
+#     dsn="https://8e73556ef21c4c73a6ecec31b9d742cc@o4504561450287104.ingest.sentry.io/4504561450287104",
+#     integrations=[
+#         DjangoIntegration(),
+#     ],
 
-    #     # Set traces_sample_rate to 1.0 to capture 100%
-    #     # of transactions for performance monitoring.
-    #     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    #     # If you wish to associate users to errors (assuming you are using
-    #     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     #     # Set traces_sample_rate to 1.0 to capture 100%
+#     #     # of transactions for performance monitoring.
+#     #     # We recommend adjusting this value in production.
+#     traces_sample_rate=1.0,
+#     #     # If you wish to associate users to errors (assuming you are using
+#     #     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
