e1afec72e94f30792d7b940e88367aa420e9d1cd
Benammar Mehdi <mehdi@benammar.com>
2023-04-28 15:30:53 +0200

    update cns prices for april 2023

--- invoices/actions/carecodes.py
+++ invoices/actions/carecodes.py
@@ -6,6 +6,47 @@ from django.db import transaction
 from invoices.models import CareCode, ValidityDate
 
 
+def update_prices_for_jan_2023(self, request, queryset):
+
+
+    # Replace 'your_csv_file.csv' with the actual path to your CSV file
+    csv_file_path = 'initialdata/2023_JAN_cns_codes.csv'
+
+    # Replace 'your_start_date' with the actual date you want to use
+    your_start_date = date(2023, 1, 1)
+    your_end_date = date(2023, 1, 31)
+
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+
+        with transaction.atomic():
+            for row in reader:
+                print(row)
+                if len(row) < 4:
+                    continue
+                care_code_str = row[1].strip()
+                new_gross_amount = float(row[3].strip())
+
+                care_code = CareCode.objects.filter(code=care_code_str).first()
+
+                if care_code:
+                    validity_date, created = ValidityDate.objects.update_or_create(
+                        care_code=care_code,
+                        end_date=None,
+                        defaults={
+                            'start_date': your_start_date,
+                            'end_date': your_end_date,
+                            'gross_amount': new_gross_amount,
+                        }
+                    )
+                    if not created:
+                        validity_date.start_date = your_start_date
+                        validity_date.end_date = your_end_date
+                        validity_date.gross_amount = new_gross_amount
+                        validity_date.save()
+
+                    print(f"Updated CareCode {care_code_str} with gross amount {new_gross_amount}")
+
 def update_prices_for_feb_2023(self, request, queryset):
 
 
@@ -14,6 +55,50 @@ def update_prices_for_feb_2023(self, request, queryset):
 
     # Replace 'your_start_date' with the actual date you want to use
     your_start_date = date(2023, 2, 1)
+    your_end_date = date(2023, 3, 31)
+
+
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+
+        with transaction.atomic():
+            for row in reader:
+                print(row)
+                if len(row) < 4:
+                    continue
+                care_code_str = row[1].strip()
+                new_gross_amount = float(row[3].strip())
+
+                care_code = CareCode.objects.filter(code=care_code_str).first()
+
+                if care_code:
+                    validity_date, created = ValidityDate.objects.update_or_create(
+                        care_code=care_code,
+                        end_date=None,
+                        defaults={
+                            'start_date': your_start_date,
+                            'end_date': your_end_date,
+                            'gross_amount': new_gross_amount,
+                        }
+                    )
+                    if not created:
+                        validity_date.start_date = your_start_date
+                        validity_date.gross_amount = new_gross_amount
+                        validity_date.end_date = your_end_date
+                        validity_date.save()
+
+                    print(f"Updated CareCode {care_code_str} with gross amount {new_gross_amount}")
+
+
+def update_prices_for_april_2023(self, request, queryset):
+
+
+    # Replace 'your_csv_file.csv' with the actual path to your CSV file
+    csv_file_path = 'initialdata/2023_FEB_cns_codes.csv'
+
+    # Replace 'your_start_date' with the actual date you want to use
+    your_start_date = date(2023, 1, 1)
+
 
     with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='"')
@@ -43,4 +128,3 @@ def update_prices_for_feb_2023(self, request, queryset):
                         validity_date.save()
 
                     print(f"Updated CareCode {care_code_str} with gross amount {new_gross_amount}")
-
