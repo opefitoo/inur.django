81748170626f1f0fe4b47cbe5a05f4fcfd4e2788
Benammar Mehdi <mehdi@benammar.com>
2023-05-10 16:16:45 +0200

    work in progress invoicing

--- dependence/activity.py
+++ dependence/activity.py
@@ -1,12 +1,41 @@
+import calendar
 import os
 
 import lxml.etree as ElementTree
 import xmlschema
 from constance import config
+from django.core.files.base import ContentFile
 from django.db import models
+from django.db.models import Count
 from django.utils.translation import gettext_lazy as _
 
 from dependence.longtermcareitem import LongTermCareItem
+
+
+def long_term_care_activity_declaration_file_path(instance, filename):
+    # Ainsi le nom des fichiers commence toujours :
+    # - par la lettre ‘D’ pour les fichiers de l’assurance dépendance
+    # − puis par le code prestataire à 8 positions
+    # − puis par l’année de décompte sur 4 positions
+    # − puis par le mois de décompte ou numéro d’envoi sur 2 positions
+    # − puis par le caractère ‘_’
+    # − puis par un identifiant convention à 3 positions qui définit une convention dans le cadre
+    # de laquelle la facturation est demandée.
+    # − puis par le caractère ‘_’
+    # − puis par le type fichier
+    # − puis par le caractère ‘_’
+    # − puis par le numéro de layout
+    # − puis par le caractère ‘_’
+    # − puis par une référence.
+    # − puis par le caractère ‘_’
+    # Illustration schématique :
+    # [F/D][Code prestataire][Année][Envoi]_[Cadre légal]_[Type Fichier]_[Numéro Layout]_[Référence]
+    # format integer to display 2 digits
+    month_of_count = f"{instance.provider_date_of_sending.month:02d}"
+    year_of_count = f"{instance.provider_date_of_sending.year:04d}"
+    newfilename = f"D{config.CODE_PRESTATAIRE}{year_of_count}{month_of_count}_ASD_DCL_001_{instance.id}{instance.version_number}.xml"
+    # newfilename, file_extension = os.path.splitext(filename)
+    return f"long_term_monthly_activity_declaration/{instance.id}/{newfilename}"
 
 
 class LongTermMonthlyActivityFile(models.Model):
@@ -18,14 +47,37 @@ class LongTermMonthlyActivityFile(models.Model):
     # invoice month
     month = models.PositiveIntegerField(_('Month'))
     provider_date_of_sending = models.DateField(_('Provider Date of Sending'))
-    file = models.FileField(upload_to='longtermmonthlyactivityfiles/')
+    force_creation = models.BooleanField(_('Force Creation'), default=False)
+    file = models.FileField(upload_to=long_term_care_activity_declaration_file_path,
+                            verbose_name=_("File"), blank=True, null=True)
     version_number = models.PositiveIntegerField(_('Version Number'))
     # Technical Fields
     created_on = models.DateTimeField("Date création", auto_now_add=True)
     updated_on = models.DateTimeField("Dernière mise à jour", auto_now=True)
     # link to the LongTermMonthlyActivity model
-    monthly_activities = models.ManyToManyField('LongTermMonthlyActivity', verbose_name=_("Activities"),
+    monthly_activities = models.ManyToManyField('LongTermMonthlyActivity',
+                                                verbose_name=_("Activities"),
+                                                help_text=_(
+                                                    "Please first save year and month to be able to add activities"),
                                                 blank=True)
+
+    def __str__(self):
+        return f"{self.file.name}"
+
+    def save(self, *args, **kwargs):
+        # if self.force_creation:
+        #     self.file.delete(save=False)
+        #     self.file = None
+        if self.force_creation and self.year and self.month:
+            self.version_number += 1
+            # generate xml data
+            xml_data = self.generate_xml_using_xmlschema()
+            if xml_data:
+                # create file
+                content_file = ContentFile(xml_data, name='long_term_care_activity.xml')
+                self.file = content_file
+                self.force_xml_generation = False
+        super().save(*args, **kwargs)
 
     def generate_xml_using_xmlschema(self):
         # Load the XSD schema file
@@ -75,14 +127,19 @@ class LongTermMonthlyActivityFile(models.Model):
             PersonneProtegee.text = activity.patient.code_sn
             ReferenceActivite = ElementTree.SubElement(Activites, "ReferenceActivite")
             ReferenceActivite.text = str(activity.id)
-            dtls = LongTermMonthlyActivityDetail.objects.filter(long_term_monthly_activity=activity).all()
-            for dtl in dtls:
+            # group by activity date
+            dtls_grouped_by_activity_date = LongTermMonthlyActivityDetail.objects.filter(
+                long_term_monthly_activity=activity).values('activity_date').annotate(
+                count=Count('activity_date')).order_by('activity_date')
+            # dtls = LongTermMonthlyActivityDetail.objects.filter(long_term_monthly_activity=activity).all()
+
+            for dtl in dtls_grouped_by_activity_date:
                 Activite = ElementTree.SubElement(Activites, "Activite")
                 AevTypeActivite = ElementTree.SubElement(Activite, "TypeActivite")
                 AevTypeActivite.text = "AEV"
                 # DateActivite
                 DateActivite = ElementTree.SubElement(Activite, "DateActivite")
-                DateActivite.text = dtl.activity_date.strftime("%Y-%m-%d")
+                DateActivite.text = dtl['activity_date'].strftime("%Y-%m-%d")
         mydata = ElementTree.tostring(root)
         if xsd_schema.is_valid(mydata):
             print("The XML instance is valid!")
@@ -106,8 +163,16 @@ class LongTermMonthlyActivity(models.Model):
     created_on = models.DateTimeField("Date création", auto_now_add=True)
     updated_on = models.DateTimeField("Dernière mise à jour", auto_now=True)
 
+    @property
+    def ratio_days_on_days_of_month(self):
+        days_in_month = calendar.monthrange(self.year, self.month)[1]
+        # number of LongTermMonthlyActivityDetail grouped by activity_date
+        count = LongTermMonthlyActivityDetail.objects.filter(long_term_monthly_activity=self).values('activity_date').annotate(
+            count=Count('activity_date')).count()
+        return "%s / %s" % (count, days_in_month)
+
     def __str__(self):
-        return f"{self.year} - {self.month}"
+        return f"{self.patient} {self.year} - {self.month}"
 
 
 class LongTermMonthlyActivityDetail(models.Model):
--- dependence/admin.py
+++ dependence/admin.py
@@ -556,7 +556,7 @@ class LongTermMonthlyActivityDetailInLine(admin.StackedInline):
 
 @admin.register(LongTermMonthlyActivity)
 class LongTermMonthlyActivityAdmin(admin.ModelAdmin):
-    list_display = ('patient', 'month', 'year')
+    list_display = ('patient', 'month', 'year', 'ratio_days_on_days_of_month')
     autocomplete_fields = ['patient']
     readonly_fields = ('created_on', 'updated_on')
     inlines = [LongTermMonthlyActivityDetailInLine]
