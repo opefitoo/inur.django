f9a19f136d6b28bb2f1a8f55a3cc4e44b861a9d0
Benammar Mehdi <mehdi@benammar.com>
2023-05-22 21:22:10 +0200

    cleanup

--- dependence/actions/monthly.py
+++ dependence/actions/monthly.py
@@ -1,11 +1,11 @@
 import calendar
-from datetime import datetime, timezone, timedelta, date
+from datetime import datetime, timezone, date
 
 from django.db import transaction
 from django.db.models import Q
 
 from dependence.activity import LongTermMonthlyActivityDetail, LongTermMonthlyActivity
-from dependence.detailedcareplan import MedicalCareSummaryPerPatient, get_summaries_between_two_dates
+from dependence.detailedcareplan import get_summaries_between_two_dates
 from dependence.invoicing import LongTermCareMonthlyStatement, LongTermCareInvoiceFile, LongTermCareInvoiceLine, \
     LongTermCareInvoiceItem
 # LongTermCareInvoiceItem
@@ -53,14 +53,6 @@ def create_packages(invoice, start_period, end_period, long_term_package):
                                                          long_term_care_package=long_term_package)
 
 
-def create_items(invoice, start_period, end_period, long_term_care_item):
-    # loop through all days of the start period and end period
-    # create an invoice item for each day
-    for day in range((end_period - start_period).days + 1):
-        start_period = start_period + timedelta(days=day)
-        return LongTermCareInvoiceItem.objects.get_or_create(invoice=invoice,
-                                                             care_date=start_period,
-                                                             long_term_care_package=long_term_care_item)
 
 
 def create_monthly_invoice_line_v2(patient, statement, month, year):
@@ -137,74 +129,3 @@ def create_monthly_invoice_items(patient, statement, month, year):
                                                                       quantity=dtl.quantity * 2)
 
 
-def create_monthly_invoice_line(patient, statement, month, year):
-    # first day of the month
-    start_period = datetime(year, month, 1)
-    # end period is last day of the month
-    last_day = calendar.monthrange(year, month)[1]
-    end_period = datetime(year, month, last_day)
-    summaries = MedicalCareSummaryPerPatient.objects.filter(patient=patient,
-                                                            date_of_decision__lte=start_period)
-    if summaries.count() != 0:
-        # create an invoice line for each activity
-        invoice, created = LongTermCareInvoiceFile.objects.get_or_create(link_to_monthly_statement=statement,
-                                                                         patient=patient,
-                                                                         invoice_start_period=start_period,
-                                                                         invoice_end_period=end_period)
-        for summary in summaries:
-            # look for level_of_needs
-            nature_package = summary.nature_package
-            if nature_package is None:
-                raise Exception('No nature package found for this summary')
-            aev_long_term_package = LongTermPackage.objects.filter(dependence_level=nature_package).get()
-            # create an invoice line for each activity
-            if summary.date_of_change_to_new_plan is not None:
-                end_period = summary.date_of_change_to_new_plan
-                start_period = summary.date_of_decision
-                # for each of period check if LongTermMonthlyActivityDetail.did_activity_happen is true
-            activity = LongTermMonthlyActivity.objects.filter(patient=patient, year=start_period.year,
-                                                              month=start_period.month).get()
-            first_date = activity.get_first_date_for_activity_detail()
-            last_day = activity.get_last_date_for_activity_detail()
-            activities = LongTermMonthlyActivityDetail.objects.filter(long_term_monthly_activity=activity,
-                                                                      activity_date__gte=start_period,
-                                                                      activity_date__lte=end_period).all()
-            cleaning_package = activities.filter(activity=LongTermCareItem.objects.filter(code='AMD-M').get()).count()
-            # if cleaning_package > 0:
-            # loop through all days from first_date to last_day
-            # for dt in range(first_date.day, last_day.day + 1):
-            # if no Hospitalization happening this day then create an invoice item
-            # if Hospitalization.filter(start_date__lte=datetime(start_period.year, start_period.month, dt),
-            #                           end_date__gte=datetime(start_period.year, start_period.month, dt),
-            #                           patient=patient).count() == 0:
-            #     item = LongTermCareInvoiceItem.objects.get_or_create(invoice=invoice,
-            #                                                               care_date=dtl.activity_date,
-            #                                                               long_term_care_package=LongTermPackage.objects.filter(
-            #                                                                   code='FAMDM').get())
-            # for dtl in activities:
-            #     if dtl.activity.code.startswith("AEV"):
-            #         item_aev = LongTermCareInvoiceItem.objects.get_or_create(invoice=invoice,
-            #                                                                  care_date=dtl.activity_date,
-            #                                                                  long_term_care_package=aev_long_term_package)
-            #     elif dtl.activity.code.startswith("AMD-M"):
-            #         item_cleaning = LongTermCareInvoiceItem.objects.get_or_create(invoice=invoice,
-            #                                                                       care_date=dtl.activity_date,
-            #                                                                       long_term_care_package=LongTermPackage.objects.filter(
-            #                                                                           code='FAMDM').get())
-
-            line_aev = LongTermCareInvoiceLine.objects.get_or_create(invoice=invoice,
-                                                                     start_period=start_period,
-                                                                     end_period=end_period,
-                                                                     long_term_care_package=aev_long_term_package)
-            line_famdm = LongTermCareInvoiceLine.objects.get_or_create(invoice=invoice,
-                                                                       start_period=start_period,
-                                                                       end_period=end_period,
-                                                                       long_term_care_package=LongTermPackage.objects.filter(
-                                                                           code='FAMDM').get())
-            line_gardes = LongTermCareInvoiceLine.objects.get_or_create(invoice=invoice,
-                                                                        start_period=start_period,
-                                                                        end_period=end_period,
-                                                                        long_term_care_package=LongTermPackage.objects.filter(
-                                                                            code='AMDGI').get())
-
-        return invoice
