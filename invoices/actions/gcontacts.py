6a0d42dcf32313545abb5a1857039937e2d0c480
Benammar Mehdi <mehdi@benammar.com>
2023-08-04 12:55:20 +0200

    fix import

--- invoices/actions/gcontacts.py
+++ invoices/actions/gcontacts.py
@@ -1,6 +1,7 @@
 import os
 
 from django.core.cache import cache
+from django_rq import job
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
