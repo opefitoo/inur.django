edecd16e9e44a40c28d4c6045a2ceb4f278b2ff8
Benammar Mehdi <mehdi@benammar.com>
2023-09-25 12:46:24 +0200

    list filter

--- dependence/admin.py
+++ dependence/admin.py
@@ -436,8 +436,8 @@ class PatientParameters(ModelAdminObjectActionsMixin, admin.ModelAdmin):
 
 @admin.register(PatientAnamnesis)
 class PatientAnamnesisAdmin(ModelAdminObjectActionsMixin, FieldsetsInlineMixin, admin.ModelAdmin):
-    list_display = ('patient', 'display_object_actions_list',)
-    list_filter = ('patient',)
+    list_display = ('patient', 'display_object_actions_list', 'created_on')
+    list_filter = ('patient','created_on')
     autocomplete_fields = ['patient']
 
     object_actions = [
