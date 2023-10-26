6c7698f273964d10eddb598956864cda6e4170cb
Benammar Mehdi <mehdi@benammar.com>
2023-10-26 11:36:52 +0200

    prepare invoices for june 2023

--- dependence/admin.py
+++ dependence/admin.py
@@ -19,7 +19,7 @@ from reportlab.pdfgen import canvas
 from dependence.aai import AAITransmission, AAITransDetail
 from dependence.actions.activity import duplicate_for_next_month, export_selected_to_csv
 from dependence.actions.initial_data import create_or_update_long_term_item_based_on_fixture
-from dependence.actions.monthly import create_aev_invoices_may_2023
+from dependence.actions.monthly import create_assurance_dependance_invoices_june_2023
 from dependence.activity import LongTermMonthlyActivity, LongTermMonthlyActivityDetail, LongTermMonthlyActivityFile
 from dependence.careplan import CarePlanDetail, CarePlanMaster, CareOccurrence
 from dependence.careplan_pdf import generate_pdf
@@ -28,7 +28,8 @@ from dependence.detailedcareplan import MedicalCareSummaryPerPatient, MedicalCar
     SharedMedicalCareSummaryPerPatientDetail
 from dependence.falldeclaration import FallDeclaration
 from dependence.forms import FallDeclarationForm, TypeDescriptionGenericInlineFormset, \
-    TensionAndTemperatureParametersFormset, CarePlanDetailForm, LongTermMonthlyActivityFileAdminForm
+    TensionAndTemperatureParametersFormset, CarePlanDetailForm, LongTermMonthlyActivityFileAdminForm, \
+    LongTermCareInvoiceLineInlineFormset
 from dependence.invoicing import LongTermCareInvoiceFile, LongTermCareInvoiceLine, LongTermCareMonthlyStatement, \
     LongTermCareInvoiceItem, LongTermCareMonthlyStatementSending
 from dependence.longtermcareitem import LongTermPackage, LongTermPackagePrice, LongTermCareItem
@@ -44,6 +45,7 @@ from invoices.models import Patient, MedicalPrescription, Bedsore
 class LongTermCareInvoiceLineInline(admin.TabularInline):
     model = LongTermCareInvoiceLine
     extra = 0
+    formset = LongTermCareInvoiceLineInlineFormset
 
 
 class LongTermCareInvoiceItemInLine(admin.TabularInline):
@@ -204,7 +206,7 @@ class MedicalCareSummaryPerPatientAdmin(admin.ModelAdmin):
               'end_of_support', 'date_of_decision', 'special_package', 'nature_package', 'cash_package',
               'fmi_right', 'sn_code_aidant', 'link_to_declaration_detail', 'date_of_notification_to_provider')
     list_filter = (FilteringPatientsForMedicalCareSummaryPerPatient, 'date_of_decision')
-    actions = [create_aev_invoices_may_2023]
+    actions = [create_assurance_dependance_invoices_june_2023]
 
     def is_latest_plan(self, obj):
         return obj.is_latest_plan
