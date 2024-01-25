7b945cb80210dcc094cee359cc2015897b7fe1ea
Benammar Mehdi <mehdi@benammar.com>
2024-01-25 17:44:11 +0100

    sync patients

---
+++ invoices/management/commands/sync_physicians.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from invoices.actions.physicians import sync_physicians_from_tsv
+
+
+class Command(BaseCommand):
+    help = 'Synchronize physicians from a TSV file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', type=str, help='The path to the TSV file')
+
+    def handle(self, *args, **options):
+        file_path = options['file_path']
+        sync_physicians_from_tsv(file_path)
+        self.stdout.write(self.style.SUCCESS('Successfully synchronized physicians'))
