6049d754a7ac15283bac2f85b9ebe96e6ee3d976
Benammar Mehdi <mehdi@benammar.com>
2024-02-07 12:23:40 +0100

    add secret clause

--- invoices/actions/certificates.py
+++ invoices/actions/certificates.py
@@ -57,6 +57,12 @@ def generate_pdf(queryset):
                                                                              w_position.number_of_hours,
                                                                              queryset[0].occupation),
                 styles['Justify']))
+            # add another paragraph that stipulates that employee needs to respect professional secrecy
+            elements.append(Spacer(2, 20))
+            elements.append(Paragraph(
+                u"L'employé est tenu de respecter le secret professionnel et de ne pas divulguer des informations "
+                u"confidentielles concernant les usagers (patients), les collègues et l'entreprise.",
+                styles['Justify']))
         elements.append(Spacer(1, 20))
     dt_now_aware = datetime.now().astimezone(ZoneInfo("Europe/Luxembourg"))  # 1
     elements.append(Paragraph(
