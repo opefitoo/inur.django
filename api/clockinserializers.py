0fda13f3dacf5ea10520dc49aa24319f513819af
Benammar Mehdi <mehdi@benammar.com>
2024-03-29 19:14:03 +0100

    fixes

--- api/clockinserializers.py
+++ api/clockinserializers.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.relations import PrimaryKeyRelatedField
 
@@ -9,12 +10,23 @@ from invoices.timesheet import SimplifiedTimesheet, SimplifiedTimesheetDetail
 
 class SimplifiedTimesheetClockInOutSerializer(serializers.ModelSerializer):
     employee = PrimaryKeyRelatedField(queryset=Employee.objects.all())
-    clock_in = serializers.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%SZ'], required=True)
+    clock_in = serializers.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%SZ'], required=False)
     clock_out = serializers.DateTimeField(input_formats=['%Y-%m-%dT%H:%M:%SZ'], required=False)
 
     class Meta:
         model = SimplifiedTimesheet
         fields = ['employee', 'time_sheet_year', 'time_sheet_month', 'clock_in', 'clock_out']
+
+    def validate(self, data):
+        clock_in = data.get('clock_in')
+        clock_out = data.get('clock_out')
+
+        if clock_in and clock_out:
+            raise serializers.ValidationError("Both clock_in and clock_out cannot be provided.")
+        if not clock_in and not clock_out:
+            raise serializers.ValidationError("Either clock_in or clock_out must be provided.")
+
+        return data
 
     def create(self, validated_data):
         employee = validated_data.get('employee')
@@ -26,27 +38,21 @@ class SimplifiedTimesheetClockInOutSerializer(serializers.ModelSerializer):
                                                        time_sheet_month=time_sheet_month).first()
         if timesheet:
             # check first if there is a detail with the same day
-            SimplifiedTimesheetDetail.objects.filter(simplified_timesheet=timesheet,
-                                                     start_date__day=clock_in.day).delete()
             current_employee_contractual_hours = employee.get_current_contract().calculate_current_daily_hours()
             # timedetla cannot be later than 22:00
             # timedetla cannot be later than 22:00
-            endtime_delta = timedelta(hours=current_employee_contractual_hours) if (clock_in + timedelta(
-                hours=current_employee_contractual_hours)) < (clock_in.replace(hour=22, minute=0,
-                                                                               second=0)) else timedelta(hours=22)
-            end_time = (clock_in + endtime_delta).time()
+            end_time = clock_in.time()
             # add a new SimplifiedTimesheetDetail with start time and end time
-            SimplifiedTimesheetDetail.objects.create(simplified_timesheet=timesheet, start_date=clock_in,
+            SimplifiedTimesheetDetail.objects.create(simplified_timesheet=timesheet,
+                                                     start_date=timezone.localtime(timezone.now()),
                                                      end_date=end_time)
         else:
             timesheet = SimplifiedTimesheet.objects.create(employee=employee, time_sheet_year=time_sheet_year,
                                                            time_sheet_month=time_sheet_month)
             current_employee_contractual_hours = employee.get_current_contract().calculate_current_daily_hours()
             # timedetla cannot be later than 22:00
-            endtime_delta = timedelta(hours=current_employee_contractual_hours) if (clock_in + timedelta(
-                hours=current_employee_contractual_hours)) < (clock_in.replace(hour=22, minute=0,
-                                                                               second=0)) else timedelta(hours=22)
-            end_time = (clock_in + endtime_delta).time()
-            SimplifiedTimesheetDetail.objects.create(simplified_timesheet=timesheet, start_date=clock_in,
+            end_time = clock_in.time()
+            SimplifiedTimesheetDetail.objects.create(simplified_timesheet=timesheet,
+                                                     start_date=timezone.localtime(timezone.now()),
                                                      end_date=end_time)
         return timesheet
--- api/clockinview.py
+++ api/clockinview.py
@@ -1,6 +1,6 @@
-from datetime import timezone
 
 from django.http import JsonResponse
+from django.utils import timezone
 from django.utils.translation import gettext as _
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -26,17 +26,22 @@ def simplified_timesheet_clock_in_view(request):
 @api_view(['POST'])
 def simplified_timesheet_clock_out_view(request):
     if request.method == 'POST':
-        # Find the most recent SimplifiedTimesheetDetail for the current user
-        timesheet_detail = SimplifiedTimesheetDetail.objects.filter(
-            simplified_timesheet__employee=Employee.objects.get(user=request.user),
-            end_date__isnull=True
-        ).order_by('-start_date').first()
-
-        if timesheet_detail:
-            # Set the end time to the current time
-            timesheet_detail.end_date = timezone.now().time()
-            timesheet_detail.save()
-            return JsonResponse({'status': 'success', 'message': _('Clock out successful')})
+        serializer = SimplifiedTimesheetClockInOutSerializer(data=request.data)
+        if not serializer.is_valid():
+            return JsonResponse({'status': 'error', 'errors': serializer.errors},
+                                status=status.HTTP_400_BAD_REQUEST)
         else:
-            return JsonResponse({'status': 'error', 'errors': 'No active timesheet found'})
-    return JsonResponse({'status': 'error', 'errors': 'Invalid request'})
+        # Find the most recent SimplifiedTimesheetDetail for the current user
+            timesheet_detail = SimplifiedTimesheetDetail.objects.filter(
+                simplified_timesheet__employee=Employee.objects.get(id=serializer.data.get('employee')),
+                start_date__date=timezone.localtime(timezone.now()).date()
+            ).order_by('-start_date').first()
+
+            if timesheet_detail:
+                # Set the end time to the current time
+                timesheet_detail.end_date = timezone.localtime(timezone.now()).time()
+                timesheet_detail.save()
+                return JsonResponse({'status': 'success', 'message': _('Clock out successful')})
+            else:
+                return JsonResponse({'status': 'error', 'errors': 'No active timesheet found'})
+        return JsonResponse({'status': 'error', 'errors': 'Invalid request'})
