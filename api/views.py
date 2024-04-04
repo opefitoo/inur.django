aead28acd3610a180aac08f443790525ecb318a9
Benammar Mehdi <mehdi@benammar.com>
2024-04-04 11:16:47 +0200

    remove caching

--- api/views.py
+++ api/views.py
@@ -819,21 +819,18 @@ def get_bank_holidays(request):
         reqs = holidays.get_bank_holidays(request.GET.get("year"), request.GET.get("month"))
         return Response(reqs, status=status.HTTP_200_OK)
 
-@cache_page(60 * 60 * 24)  # Cache page for 24 hours
 @api_view(['GET'])
 def how_many_care_given(request):
     if 'GET' == request.method:
         reqs = Prestation.objects.all().count() + Event.objects.filter(state=3).count()
         return Response(reqs, status=status.HTTP_200_OK)
 
-@cache_page(60 * 60 * 24)
 @api_view(['GET'])
 def how_many_patients(request):
     if 'GET' == request.method:
         reqs = Patient.objects.all().count()
         return Response(reqs, status=status.HTTP_200_OK)
 
-@cache_page(60 * 60 * 24)  # Cache page for 24 hours
 @api_view(['GET'])
 def how_many_care_hours(request):
     if 'GET' == request.method:
