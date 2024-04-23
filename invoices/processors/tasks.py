afe1cf0a14a395d70d3e73761d8eb9d18f90a8d8
Benammar Mehdi <mehdi@benammar.com>
2024-04-23 11:56:28 +0200

    handle errors

--- invoices/processors/tasks.py
+++ invoices/processors/tasks.py
@@ -20,61 +20,66 @@ from invoices.prefac import generate_all_invoice_lines
 
 @job
 def process_post_save(instance):
-    # calculate how much time it takes to process the batch
-    start = datetime.now()
-    _must_update = False
+    try:
+        # calculate how much time it takes to process the batch
+        start = datetime.now()
+        _must_update = False
 
-    if instance.force_update:
-        _must_update = True
-        instance.version += 1
-        instance.force_update = False
-    if _must_update:
-        from invoices.models import InvoiceItem
-        # Now update all InvoiceItems which have an invoice_date within this range
-        # if BatchTypeChoices.CNS_INF == instance.batch_type:
-        # batch_invoices = InvoiceItem.objects.filter(
-        #     Q(invoice_date__gte=instance.start_date) & Q(invoice_date__lte=instance.end_date)).filter(
-        #     invoice_sent=False).filter(batch__isnull=True)
-        # batch_invoices.update(batch=instance)
-        batch_invoices = InvoiceItem.objects.filter(batch=instance).annotate(
-            is_under_dependence_insurance_order=Case(
-                When(patient__is_under_dependence_insurance=False, then=Value(0)),
-                When(patient__is_under_dependence_insurance=True, then=Value(1)),
-                default=Value(2),
-                output_field=IntegerField(),
-            )).order_by('is_under_dependence_insurance_order', 'patient_id')
-        file_content = generate_all_invoice_lines(batch_invoices, invoice_batch_date=instance.end_date,
-                                                  batch_type=instance.batch_type)
-        instance.prefac_file = ContentFile(file_content.encode('utf-8'), 'prefac.txt')
+        if instance.force_update:
+            _must_update = True
+            instance.version += 1
+            instance.force_update = False
+        if _must_update:
+            from invoices.models import InvoiceItem
+            # Now update all InvoiceItems which have an invoice_date within this range
+            # if BatchTypeChoices.CNS_INF == instance.batch_type:
+            # batch_invoices = InvoiceItem.objects.filter(
+            #     Q(invoice_date__gte=instance.start_date) & Q(invoice_date__lte=instance.end_date)).filter(
+            #     invoice_sent=False).filter(batch__isnull=True)
+            # batch_invoices.update(batch=instance)
+            batch_invoices = InvoiceItem.objects.filter(batch=instance).annotate(
+                is_under_dependence_insurance_order=Case(
+                    When(patient__is_under_dependence_insurance=False, then=Value(0)),
+                    When(patient__is_under_dependence_insurance=True, then=Value(1)),
+                    default=Value(2),
+                    output_field=IntegerField(),
+                )).order_by('is_under_dependence_insurance_order', 'patient_id')
+            file_content = generate_all_invoice_lines(batch_invoices, invoice_batch_date=instance.end_date,
+                                                      batch_type=instance.batch_type)
+            instance.prefac_file = ContentFile(file_content.encode('utf-8'), 'prefac.txt')
 
-        # generate the pdf invoice file
-        # Create a BytesIO buffer
-        buffer = BytesIO()
-        doc = SimpleDocTemplate(buffer, rightMargin=2 * cm, leftMargin=2 * cm, topMargin=1 * cm,
-                                bottomMargin=1 * cm)
-        elements, copies_of_medical_prescriptions = get_doc_elements(batch_invoices, med_p=True,
-                                                                     with_verification_page=True, batch_file=instance)
-        doc.build(elements)
-        # Go to the beginning of the buffer
-        buffer.seek(0)
-        instance.generated_invoice_files = ContentFile(buffer.read(), 'invoice.pdf')
+            # generate the pdf invoice file
+            # Create a BytesIO buffer
+            buffer = BytesIO()
+            doc = SimpleDocTemplate(buffer, rightMargin=2 * cm, leftMargin=2 * cm, topMargin=1 * cm,
+                                    bottomMargin=1 * cm)
+            elements, copies_of_medical_prescriptions = get_doc_elements(batch_invoices, med_p=True,
+                                                                         with_verification_page=True, batch_file=instance)
+            doc.build(elements)
+            # Go to the beginning of the buffer
+            buffer.seek(0)
+            instance.generated_invoice_files = ContentFile(buffer.read(), 'invoice.pdf')
 
-        merger = PdfMerger()
-        for file in copies_of_medical_prescriptions:
-            merger.append(file)
-        pdf_buffer = BytesIO()
-        merger.write(pdf_buffer)
-        pdf_buffer.seek(0)
-        instance.medical_prescriptions = ContentFile(pdf_buffer.read(), 'ordos.pdf')
-        instance.save()
-        end = datetime.now()
-        if os.environ.get('LOCAL_ENV', None):
-            print("Batch {0} processed in {1} seconds".format(instance, (end - start).seconds))
-        else:
-            url = config.ROOT_URL + '/admin/invoices/invoiceitembatch/?id=' + '{0}'.format(instance.id)
-            notify_system_via_google_webhook(
-                "Batch {0} processed in {1} seconds click on link to check {2}".format(instance, (end - start).seconds,
-                                                                                   url))
+            merger = PdfMerger()
+            for file in copies_of_medical_prescriptions:
+                merger.append(file)
+            pdf_buffer = BytesIO()
+            merger.write(pdf_buffer)
+            pdf_buffer.seek(0)
+            instance.medical_prescriptions = ContentFile(pdf_buffer.read(), 'ordos.pdf')
+            instance.save()
+            end = datetime.now()
+            if os.environ.get('LOCAL_ENV', None):
+                print("Batch {0} processed in {1} seconds".format(instance, (end - start).seconds))
+            else:
+                url = config.ROOT_URL + '/admin/invoices/invoiceitembatch/?id=' + '{0}'.format(instance.id)
+                notify_system_via_google_webhook(
+                    "Batch {0} processed in {1} seconds click on link to check {2}".format(instance, (end - start).seconds,
+                                                                                       url))
+    except Exception as e:
+        error_detail = traceback.format_exc()
+        notify_system_via_google_webhook(
+            "*An error occurred while processing the batch: {0}*\nDetails:\n{1}".format(e, error_detail))
 
 @job("default", timeout=6000)
 def duplicate_event_for_next_day_for_several_events(events, who_created, number_of_days=1):
