b5e515ff5eda104077f62b97b51cfc67f669daec
Benammar Mehdi <mehdi@benammar.com>
2024-04-25 17:40:32 +0200

    display when it was duplicated

--- invoices/admin.py
+++ invoices/admin.py
@@ -1774,7 +1774,7 @@ class EventAdmin(admin.ModelAdmin):
 
 @admin.register(EventList)
 class EventListAdmin(admin.ModelAdmin):
-    list_display = ['day', 'time_start_event', 'time_end_event', 'patient', 'employees', 'state']
+    list_display = ['day', 'time_start_event', 'time_end_event', 'patient', 'employees', 'state', 'created_on']
     change_list_template = 'admin/change_list.html'
     list_filter = (
         'employees', 'event_type_enum', 'state', SmartPatientFilter, 'created_by', UnderAssuranceDependanceFilter)
