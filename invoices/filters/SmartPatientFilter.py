d5b2c41ed8205b5163651776fb42d0c358fa2ba8
Benammar Mehdi <mehdi@benammar.com>
2024-06-20 18:58:40 +0200

    heatwave risk

--- invoices/filters/SmartPatientFilter.py
+++ invoices/filters/SmartPatientFilter.py
@@ -17,3 +17,40 @@ class UnderAssuranceDependanceFilter(admin.SimpleListFilter):
             return queryset.filter(patient__is_under_dependence_insurance=True)
         elif self.value() == 'no':
             return queryset.filter(patient__is_under_dependence_insurance=False)
+
+class UnderHeatWaveRiskFilter(admin.SimpleListFilter):
+    title = _('Under Heat Wave Risk')
+    parameter_name = 'under_heat_wave_risk'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('>75', _('Older than 75')),
+            ('<=75', _('75 or younger')),
+            ('unknown', _('Unknown Age')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '>75':
+            return queryset.filter(pk__in=[patient.pk for patient in queryset if patient.age is not None and patient.age > 75])
+        if self.value() == '<=75':
+            return queryset.filter(pk__in=[patient.pk for patient in queryset if patient.age is not None and patient.age <= 75])
+        if self.value() == 'unknown':
+            return queryset.filter(pk__in=[patient.pk for patient in queryset if patient.age is None])
+        return queryset
+
+class IsPatientDeceasedFilter(admin.SimpleListFilter):
+    title = _('Is Patient Deceased')
+    parameter_name = 'is_patient_deceased'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', _('Yes')),
+            ('no', _('No')),
+        )
+
+    # only if date_of_death is not null
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(date_of_death__isnull=False)
+        elif self.value() == 'no':
+            return queryset.filter(date_of_death__isnull=True)
