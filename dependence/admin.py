247e262e6f69afec9171b7c1394a2a06f225f29d
Benammar Mehdi <mehdi@benammar.com>
2024-10-22 16:08:20 +0200

    connect to local db

--- dependence/admin.py
+++ dependence/admin.py
@@ -1014,7 +1014,7 @@ class LongTermMonthlyActivityAdmin(admin.ModelAdmin):
 
 @admin.register(LongTermMonthlyActivityFile)
 class LongTermMonthlyActivityFileAdmin(admin.ModelAdmin):
-    list_display = ('month', 'year', 'has_errors_in_return_file', 'has_return_file')
+    list_display = ('month', 'year', 'has_return_file')
     list_filter = ('month', 'year')
     readonly_fields = ('created_on', 'updated_on')
     form = LongTermMonthlyActivityFileAdminForm
